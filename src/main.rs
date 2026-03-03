dfe69e4531c9152e0ef5c5f9f6bc7a19c9c2bb3f
theawakener0 <wakeupwithme000@gmail.com>
2026-03-04 00:30:34 +0200

    ADDED Reddit Fetch...

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,28 @@ struct HNStory {
     score: Option<u32>,
 }
 
+#[derive(Debug, Deserialize)]
+struct RedditResponse {
+    data: RedditData,
+}
+
+#[derive(Debug, Deserialize)]
+struct RedditData {
+    children: Vec<RedditPost>,
+}
+
+#[derive(Debug, Deserialize)]
+struct RedditPost {
+    data: PostData,
+}
+
+#[derive(Debug, Deserialize)]
+struct PostData {
+    title: String,
+    score: u32,
+    url: String,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     
@@ -16,22 +38,32 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     banner();
 
-    let response = fetch_top_ids_hn(&client).await.unwrap();
+    let response_hn = fetch_top_ids_hn(&client).await.unwrap();
 
-    for id in response.iter().take(10) {
+    for id in response_hn.iter().take(10) {
         match fetch_story_hn(&client, *id).await {
             Ok(story) => {
-                println!("| {:-<30} |", "");
                 println!("| Title: {:.<26} |", story.title.as_deref().unwrap_or("N/A"));
                 println!("| URL:   {:.<26} |", story.url.as_deref().unwrap_or("N/A"));
                 println!("| Score: {:.<26} |", story.score.map(|s| s.to_string()).unwrap_or_else(|| "N/A".to_string()));
-                println!("| {:-<30} |", "");
+                println!("| {:-<30} ", "");
             }
             Err(e) => {
                 eprintln!("Failed to fetch story {}: {}", id, e);
             }
         }
     }
+
+    let response_rd = fetch_from_subreddit(&client, "rust", 10).await.unwrap();
+
+    for post in response_rd.data.children.iter() {
+        println!("| Title: {:.<26} |", post.data.title);
+        println!("| URL:   {:.<26} |", post.data.url);
+        println!("| UpVotes: {:.<26} |", post.data.score);
+        println!("| {:-<30} ", "");
+    }
+
+
     
     Ok(())
 
@@ -76,6 +108,17 @@ async fn fetch_story_hn(client: &Client, id: u64) -> Result<HNStory, reqwest::Er
         .send()
         .await?
         .json::<HNStory>()
+        .await
+}
+
+async fn fetch_from_subreddit(client: &Client, subreddit: &str, lim: u32) -> Result<RedditResponse, reqwest::Error> {
+    let url = format!("https://www.reddit.com/r/{}/top.json?limit={}", subreddit, lim);
+    client
+        .get(&url)
+        .header("User-Agent", "trendy-cli")
+        .send()
+        .await?
+        .json::<RedditResponse>()
         .await
 }
 
