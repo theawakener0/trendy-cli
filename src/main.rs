90f4a1768df8cdc0a5d762a9b94f6d5fb52b4a6c
theawakener0 <wakeupwithme000@gmail.com>
2026-03-05 17:01:01 +0200

    ADDED REPL mode...

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::{Write, stdin, stdout};
+use std::process;
 use reqwest::Client;
 use clap::{Parser};
 use crate::fetch::hn::{fetch_top_ids_hn, fetch_story_hn};
@@ -60,9 +62,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     println!("| Score: {:.<26} |", story.score.map(|s| s.to_string()).unwrap_or_else(|| "N/A".to_string()));
                     println!("| {:-<30} ", "");
                     println!("{}", RESET);
+                    process::exit(1);    
                 }
                 Err(e) => {
                     eprintln!("{}Failed to fetch story {}: {}{}",RED, id, e, RESET);
+                    process::exit(1);    
                 }
             }
         }
@@ -78,6 +82,113 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             println!("| {:-<30} ", "");
             println!("{}", RESET);
         }
+        process::exit(1);    
+    }
+
+    let err = format!("{}Try Again{}",RED, RESET);
+    
+    loop {
+        
+        let mut line = String::new();
+
+        print!("{}► {}", ORANGE, RESET);
+        stdout().flush()?;
+
+        stdin()
+            .read_line(&mut line)
+            .expect(&err);
+        let line_trim = line.trim();
+
+        if line_trim == "/quit" {
+            println!("{}\n{} Thanks for using Trendy! Goodbye!  {}\n", CLEAR, ORANGE, RESET);
+            break;
+        }
+        else if line_trim == "/clear" {
+            println!("{}", CLEAR);
+        }
+        else if line_trim == "/rd" {
+            let mut subreddit = String::new();
+            let mut limit = String::new();
+            let rd_flag: bool = true;
+
+            print!("{}[/r/]► {}", ORANGE, RESET);
+            stdout().flush()?;
+
+            stdin()
+                .read_line(&mut subreddit)
+                .expect(&err);
+            
+            print!("{}[limit]► {}", ORANGE, RESET);
+            stdout().flush()?;
+
+            stdin()
+                .read_line(&mut limit)
+                .expect(&err);
+            let limit_usize: usize = limit
+                .trim()
+                .parse()
+                .expect(&err);
+
+            let rd = if rd_flag {
+                Some(fetch_from_subreddit(&client, subreddit, limit_usize).await?)
+            } else {
+                None
+            };
+            if let Some(rd) = rd {
+                for post in rd.data.children.iter() {
+                    println!("{}", ORANGE);
+                    println!("| Title: {:.<26} |", post.data.title.as_deref().unwrap_or("N/A"));
+                    println!("| URL:   {:.<26} |", post.data.url.as_deref().unwrap_or("N/A"));
+                    println!("| UpVotes: {:.<26} |", post.data.score);
+                    println!("| {:-<30} ", "");
+                    println!("{}", RESET);
+                }
+            }
+
+        }
+        else if line_trim == "/hn" {
+            let mut limit = String::new();
+            let hn_flag: bool = true;
+
+            print!("{}[limit]► {}", ORANGE, RESET);
+            stdout().flush()?;
+
+            stdin()
+                .read_line(&mut limit)
+                .expect(&err);
+            let limit_usize: usize = limit
+                .trim()
+                .parse()
+                .expect(&err);
+
+            let hn = if hn_flag {
+                Some(fetch_top_ids_hn(&client).await?)
+            } else {
+                None
+            };
+            if let Some(hn) = hn {
+                for id in hn.iter().take(limit_usize) {
+                    match fetch_story_hn(&client, *id).await {
+                        Ok(story) => {
+                            println!("{}", ORANGE);
+                            println!("| Title: {:.<26} |", story.title.as_deref().unwrap_or("N/A"));
+                            println!("| URL:   {:.<26} |", story.url.as_deref().unwrap_or("N/A"));
+                            println!("| Score: {:.<26} |", story.score.map(|s| s.to_string()).unwrap_or_else(|| "N/A".to_string()));
+                            println!("| {:-<30} ", "");
+                            println!("{}", RESET);
+                            process::exit(1);    
+                        }
+                        Err(e) => {
+                            eprintln!("{}Failed to fetch story {}: {}{}",RED, id, e, RESET);
+                            process::exit(1);    
+                        }
+                    }
+                }
+            }
+
+        }
+
+
     }
 
 
